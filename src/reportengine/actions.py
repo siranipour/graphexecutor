323750b9e8744604dd2646af5aca04ad1d5ff227
siranipour <si292@cam.ac.uk>
2021-04-07 11:58:27 +0100

    Explaining actions a bit better

--- src/reportengine/actions.py
+++ src/reportengine/actions.py
@@ -7,11 +7,13 @@ def multiply_three(num):
     return num * 3
 
 def action(add_one, multiply_three):
-    """f: x |-> 1 + x + 3x
+    """f: x |-> 1 + x + 3x = 1+4x
     """
     return add_one + multiply_three
 
 def more_complicated_action(action, add_one):
+    """ Returns (1+x)^2 + 1+4x = x^2 + 6x + 2
+    """
     return action + add_one ** 2
 
 def some_other_action(more_complicated_action, foo):
